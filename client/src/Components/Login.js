7a26264ef255c95a2570de2be16527119a7282a8
Aditya Srivastava <adityaa11233@gmail.com>
2022-10-15 15:49:05 +0530

    fixes

--- client/src/Components/Login.js
+++ client/src/Components/Login.js
@@ -9,7 +9,7 @@ const Login = () => {
         email: "",
         password: ""
     })
-    
+
     const [passwordType, setPasswordType] = useState("password");
     const handleChange = (e) => {
         const { name, value } = e.target
@@ -26,10 +26,10 @@ const Login = () => {
                     console.log(res)
                     navigate("/homepage")
                 })
-        }   
+        }
     }
 
-    const register=()=>navigate("/signup")
+    const register = () => navigate("/signup")
 
     return (
         <>
@@ -42,10 +42,10 @@ const Login = () => {
                             <h2 className="font-bold text-2xl text-[#002D74]">Login</h2>
                             <p className="text-xs mt-4 text-[#002D74]">If you are already a member, easily log in</p>
 
-                            <form onSubmit={(e) => e.preventDefault() } action="" className="flex flex-col gap-4">
-                                <input className="p-2 mt-8 rounded-xl border" type="email" name = "email" value={user.email} onChange = {handleChange} placeholder="Email" />
+                            <form onSubmit={(e) => e.preventDefault()} action="" className="flex flex-col gap-4">
+                                <input className="p-2 mt-8 rounded-xl border" type="email" name="email" value={user.email} onChange={handleChange} placeholder="Email" />
                                 <div className="relative">
-                                    <input className="p-2 rounded-xl border w-full" type={passwordType} name = "password" value={ user.password } onChange = {handleChange} placeholder="Password" />
+                                    <input className="p-2 rounded-xl border w-full" type={passwordType} name="password" value={user.password} onChange={handleChange} placeholder="Password" />
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="gray" className="bi bi-eye absolute top-1/2 right-3 -translate-y-1/2" viewBox="0 0 16 16"
                                         onClick={() => {
                                             if (passwordType === "password") {
@@ -53,7 +53,7 @@ const Login = () => {
                                             } else {
                                                 setPasswordType("password")
                                             }
-                                        } }>
+                                        }}>
                                         <path d="M16 8s-3-5.5-8-5.5S0 8 0 8s3 5.5 8 5.5S16 8 16 8zM1.173 8a13.133 13.133 0 0 1 1.66-2.043C4.12 4.668 5.88 3.5 8 3.5c2.12 0 3.879 1.168 5.168 2.457A13.133 13.133 0 0 1 14.828 8c-.058.087-.122.183-.195.288-.335.48-.83 1.12-1.465 1.755C11.879 11.332 10.119 12.5 8 12.5c-2.12 0-3.879-1.168-5.168-2.457A13.134 13.134 0 0 1 1.172 8z" />
                                         <path d="M8 5.5a2.5 2.5 0 1 0 0 5 2.5 2.5 0 0 0 0-5zM4.5 8a3.5 3.5 0 1 1 7 0 3.5 3.5 0 0 1-7 0z" />
                                     </svg>
@@ -79,7 +79,7 @@ const Login = () => {
 
                             <div className="mt-5 text-xs border-b border-[#002D74] py-4 text-[#002D74]">
                                 {/*add link tag here */}
-                                 Forgot your password? 
+                                Forgot your password?
                             </div>
 
                             <div className="mt-3 text-xs flex justify-between items-center text-[#002D74]">
@@ -92,7 +92,7 @@ const Login = () => {
                     </div>
                 </section>
             </div>
-            
+
         </>
     )
 }
